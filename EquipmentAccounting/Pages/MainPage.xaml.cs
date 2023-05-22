b6e1f1ec62dd52a58108903cb35e98f10a45cb8a
Hawinar <egorchelnokov@bk.ru>
2023-05-22 20:48:03 +0300

    Уменьшение кода

--- EquipmentAccounting/Pages/MainPage.xaml.cs
+++ EquipmentAccounting/Pages/MainPage.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using EquipmentAccounting.Models;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EquipmentAccounting.Pages
 {
@@ -22,7 +10,7 @@ namespace EquipmentAccounting.Pages
     /// </summary>
     public partial class MainPage : Page
     {
-      
+
         private static Frame MainFrame;
 
         public MainPage(Frame mainFrame)
@@ -41,7 +29,7 @@ namespace EquipmentAccounting.Pages
 
             int targetId = d.Id;
 
-            
+
             EquipmentDBEntities.GetEntities().Equipment.Remove(d);
             EquipmentDBEntities.GetEntities().LocationHistory.RemoveRange(EquipmentDBEntities.GetEntities().LocationHistory.Where(x => x.EquipmentId == targetId).ToList());
             EquipmentDBEntities.GetEntities().StatusHistory.RemoveRange(EquipmentDBEntities.GetEntities().StatusHistory.Where(x => x.EquipmentId == targetId).ToList());
